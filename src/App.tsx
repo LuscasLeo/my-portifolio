acc5a054a8ae624bb3bd1c8681fc7cce67f6306b
Lucas Silva <lucas.silva@fontespromotora.com.br>
2022-02-25 00:30:37 -0300

    style: Ajuste de estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import './styles.scss'
+import "./styles.scss";
 function App() {
-
   return (
     <code lang="typescript">
-      
       Olá, meu nome é Lucas Silva <s>pode me chamar de lusca</s>
       <br />
       tenho 22 anos
-
       <br />
-
       <br />
       Sou programador backend <s>notoriamente</s>
-      
+      <br />
+      <a target="_blank" href="https://www.linkedin.com/in/luscasleodev/">Esse é meu linkedin</a>
+      <br />
+      <a target="_blank" href="https://github.com/LuscasLeo/">Esse é meu github</a>
     </code>
-  )
+  );
 }
 
-export default App
+export default App;
